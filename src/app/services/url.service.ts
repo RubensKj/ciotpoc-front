6936069a1c35bf52772193d72b8777796ef0d1a0
rubenskleinschmidtjr@gmail.com <pacoloco09>
2020-03-06 10:41:43 -0300

    Adding environment variables

--- src/app/services/url.service.ts
+++ src/app/services/url.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 
+import { environment } from 'src/environments/environment.prod';
+// import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UrlService {
 
-  baseUrl = 'http://localhost:8080/api';
+  baseUrl = environment.baseUrlAPI;
 
   constructor() { }
 
